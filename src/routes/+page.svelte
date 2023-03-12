d1b66357eefafaba85a152cc6e1137d800adc509
G-yhlee <yhleesoft@gmail.com>
2023-03-12 22:37:06 +0900

    bind window

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+    import { innerWidth } from "../store/store";
+
+</script>
+<svelte:window bind:innerWidth={$innerWidth} />
+
+<p>The window inner width is {$innerWidth}</p>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
